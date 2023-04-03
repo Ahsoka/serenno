6d26097e0b65c91531c127e3ed6a376173b402a9
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2023-04-03 02:07:24 -0700

    Added image updating depending on the active workspace
    Closes #1

--- listener.py
+++ listener.py
@@ -1,5 +1,5 @@
-from .utils import ProductType, WrongWorkspace
-from adsk.core import Application, Document
+from .utils import WorkspaceEnum, ProductType, WrongWorkspace
+from adsk.core import Application, Document, Workspace
 from .pypresence.pypresence import Presence
 from .fusion360utils import add_handler
 from adsk.fusion import Design
@@ -54,7 +54,7 @@ class Listener(Presence):
         self.app = app
         self.current_state['start'] = int(time.time())
         self.document_change(self.app.activeDocument, update=False)
-        self.workspace_change(self.app.userInterface.activeWorkspace.name, update=False)
+        self.workspace_change(self.app.userInterface.activeWorkspace, update=False)
 
         add_handler(
             app.documentActivating,
@@ -62,7 +62,7 @@ class Listener(Presence):
         )
         add_handler(
             app.userInterface.workspacePreActivate,
-            lambda workspace_event: self.workspace_change(workspace_event.workspace.name)
+            lambda workspace_event: self.workspace_change(workspace_event.workspace)
         )
 
         self.connect()
@@ -97,8 +97,22 @@ class Listener(Presence):
         if update and self.connected:
             self.update()
 
-    def workspace_change(self, workspace_name: str, update: bool = True):
+    def workspace_change(self, workspace: Workspace, update: bool = True):
+        workspace_name = workspace.name
         self.current_state['state'] = f"Workspace: {workspace_name}"
-        # TODO: Add image update logic.
+
+        for workspace_enum in WorkspaceEnum:
+            if workspace.id == workspace_enum:
+                # NOTE: Transforms enum name from GENERATIVE_DESIGN -> generative-design-workspace
+                large_image = f"{workspace_enum.name.lower().replace('_', '-')}-workspace"
+                self.current_state['large_image'] = large_image
+                self.current_state['small_image'] = 'fusion-360-icon'
+                self.current_state['small_text'] = 'Autodesk Fusion 360'
+                break
+        else:
+            self.current_state['large_image'] = 'fusion-360-icon-borderless'
+            self.current_state['small_image'] = None
+            self.current_state['small_text'] = None
+
         if update and self.connected:
             self.update()
