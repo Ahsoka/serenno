d1af2a8fc2fd74e980ea4a95f66b33b8d39b729e
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2023-04-03 02:04:50 -0700

    Created WorkspaceEnum enum in utils.py

--- utils.py
+++ utils.py
@@ -9,5 +9,19 @@ class ProductType(str, Enum):
     DRAWING = "FusionDrawingProductType"
 
 
+class WorkspaceEnum(str, Enum):
+    DESIGN = "FusionSolidEnvironment"
+    # NOTE: Have not tested GENERATIVE_DESIGN since I don't have access to it
+    # id is sourced from this screenshot:
+    # https://help.autodesk.com/cloudhelp/ENU/Fusion-360-API/images/UIExport1.png
+    GENERATIVE_DESIGN = "GenartiveEnvironment"
+    RENDER = "FusionRenderEnvironment"
+    ANIMATION = "Publisher3DEnvironment"
+    SIMULATION = "SimulationEnvironment"
+    MANUFACTURE = "CAMEnvironment"
+    DRAWING = "FusionDocumentationEnvironment"
+    PCB = "PCBDesignEnvironement"
+
+
 class WrongWorkspace(Exception):
     pass
