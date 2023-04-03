b81253595bbb84cf16ad1d9e623f20d2d4912f8d
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2023-04-03 00:04:28 -0700

    Created ProductType Enum in utils.py

---
+++ utils.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class ProductType(str, Enum):
+    DESIGN = "DesignProductType"
+    ANIMATION = "AnimationProductType"
+    SIMULATION = "SimStudiesProductType"
+    MANUFACTURE = "CAMProductType"
+    DRAWING = "FusionDrawingProductType"
