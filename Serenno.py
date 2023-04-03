d564cf41b979c9d12aabcd4798a3ce56e8bd06ed
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2023-04-02 20:35:11 -0700

    Destroy listener when the app is shutting down

--- Serenno.py
+++ Serenno.py
@@ -4,3 +4,8 @@ from .init import listener
 def run(context: dict):
     app = Application.get()
     listener.init(app)
+
+def stop(context: dict):
+    # TODO: Added error handling in the stop function;
+    # any errors that happen here are not recorded on the console
+    listener.destroy()
